0f55a2b32c85329e68f02626105cd814249860f4
Kwag De Gi <code@skyaperture.com>
2015-01-21 23:34:48 +0900

    linked list set

---
+++ 0121/link.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+
+struct node {
+	int x;
+	struct node *next;
+};
+
+int main() 
+{
+	struct node *root;
+
+	root = (struct node *) malloc( sizeof(struct node));
+
+	root->next = 0;
+
+	root->x =5;
+
+	return 0;
+}
