c1610d34198c06e043cb71f27f6bf095472916a2
Kwag De Gi <code@skyaperture.com>
2015-02-09 08:16:45 +0900

    malloc pointer

---
+++ 0209/malloc2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main()
+{
+	char *str;
+
+	str = (char *) malloc(15);
+	strcpy(str, "degi is genius");
+	printf("String = %s, Address = %u\n", str, *str);
+
+	str = (char *) realloc(str, 25);
+	strcat(str, ".com");
+	printf("String = %s, Address = %u\n", str, *str);
+
+	free(str);
+	
+	return 0;
+}
