de05eee2ab204211be916fece72f68008222c17c
Kwag De Gi <code@skyaperture.com>
2015-02-14 07:03:10 +0900

    pointer arithmetic

---
+++ 0214/pa2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+	int i,j;
+	int arr[10];
+	for ( i = 0; i < 10; i++ )
+	{
+		arr[i] = i;
+	}
+	for ( j = 1; j <= 10; j++ )
+	{
+		if ( arr[j] == *(arr+j) )
+		{	
+			printf("This is %d\n", arr[j]);
+		} else {
+			printf("End of the loop.\n");
+		}
+	}
+
+	return 0;
+}
